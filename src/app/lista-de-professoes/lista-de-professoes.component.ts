8fb631f698746af3ab720cb73765cbf911068aab
Yuri Ramalho Silva <yuri.silva@sysmap.com.br>
2020-10-07 19:56:58 -0300

    estilização dos botões angular

--- src/app/lista-de-professoes/lista-de-professoes.component.ts
+++ src/app/lista-de-professoes/lista-de-professoes.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-lista-de-professoes',
   templateUrl: './lista-de-professoes.component.html',
   styleUrls: ['./lista-de-professoes.component.css']
 })
-export class ListaDeProfessoesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ListaDeProfessoesComponent {
 
 }
